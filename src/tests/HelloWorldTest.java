11c1500f4c588b4ffdc353ee91057f21684e8aff
Shirin Geldiyeva <shirin7271@gmail.com>
2021-10-04 01:15:50 -0400

    updated hello world class

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -4,5 +4,7 @@ public class HelloWorldTest {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        System.out.println("One more hello world");
     }
 }
