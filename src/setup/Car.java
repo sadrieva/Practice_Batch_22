084fc0768ae482c20dd950463af116fc21120cd9
Shirin Geldiyeva <shirin7271@gmail.com>
2021-10-04 00:57:46 -0400

    updating master origin

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -1,18 +1,18 @@
 package setup;
 
 public class Car {
-<<<<<<< HEAD
+
     //this is car class from master branch
 
     // we will conflict it
 
     // because there is different text in different branches
-=======
+
 
     // this is our car from james branch
 
     //more text
 
     //more text
->>>>>>> james
+
 }
